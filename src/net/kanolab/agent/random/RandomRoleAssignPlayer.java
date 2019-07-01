da928a6ad912ecc99ef731be98492aee586d9e50
ymaki <ymaki@freccia>
2019-07-01 15:14:59 +0900

    second

---
+++ src/net/kanolab/agent/random/RandomRoleAssignPlayer.java
@@ -0,0 +1,13 @@
+package net.kanolab.agent.random;
+
+import org.aiwolf.sample.lib.AbstractRoleAssignPlayer;
+//import org.aiwolf.sample.player.*;
+
+public class RandomRoleAssignPlayer extends AbstractRoleAssignPlayer {
+	public String getName() {
+		return "RandomTalkPlayer";
+	}
+	public static void main(String args[]) {
+		System.out.println("success");
+	}
+}
